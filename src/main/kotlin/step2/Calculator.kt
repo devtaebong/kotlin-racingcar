1707f5d5af84b3e4d64ea017634c33585db2fb5c
bird798 <bird798@gmail.com>
2024-11-14 09:39:43 +0900

    Step2 (#1603)
    * Test
    
    * 문자열 사칙 연산 계산기 구현
    
    * 불필요한 파일 삭제
    
    * * Testcase
    - DisplayName추가.
    - 반복되는 단언문 삭제
    
    * Calculator
    - 열거형에 함수를 대입하여 When문을 대체
    - 구분문자(" ")를 상수로 정의
    - 불필요한 주석 제거
    
    * 1. 변수명 변경
    2. Error Message 상수값으로 변경
    3. 중복된 에러 처리 삭제
    
    ---------
    
    Co-authored-by: 박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>

---
+++ src/main/kotlin/step2/Calculator.kt
@@ -0,0 +1,48 @@
+package step2
+
+class Calculator() {
+    enum class OPERATOR(val operator: String, val apply: (Double, Double) -> Double) {
+        PLUS("+", { a, b -> a + b }),
+        MINUS("-", { a, b -> a - b }),
+        MULTIPLY("*", { a, b -> a * b }),
+        DIVIDE("/", { a, b -> a / b }),
+        ;
+
+        companion object {
+            private val map = entries.associateBy(OPERATOR::operator)
+
+            fun get(operation: String): OPERATOR? {
+                return map[operation]
+            }
+        }
+    }
+
+    companion object {
+        const val SEPARATOR = " "
+
+        const val ERROR_MESSAGE_ILLEGAL_FORMULAS = "정의되지 않은 연산자입니다."
+        const val ERROR_MESSAGE_ILLEGAL_OPERATOR = "정의되지 않은 연산자입니다."
+        const val ERROR_MESSAGE_ILLEGAL_OPERAND = "숫자가 아닌값이 입력되었습니다."
+    }
+
+    fun calculate(formulas: String): Double {
+        val factors = split(formulas)
+        if (factors.isEmpty() || (factors.size % 2 != 1)) {
+            throw IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_FORMULAS)
+        }
+
+        var result = factors[0].toDoubleOrNull() ?: throw IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_OPERAND)
+        for (i in 1 until factors.size step 2) {
+            val operator = OPERATOR.get(factors[i]) ?: throw IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_OPERATOR)
+            val operand = factors[i + 1].toDoubleOrNull() ?: throw IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_OPERAND)
+            result = operator.apply(result, operand)
+        }
+
+        return result
+    }
+
+    private fun split(formulas: String): List<String> {
+        val factors = formulas.trim().split(SEPARATOR)
+        return factors
+    }
+}
---
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -0,0 +1,36 @@
+package step2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+
+class CalculatorTest {
+    @ParameterizedTest
+    @CsvSource(
+        "'1.0', 1.0",
+        "'1.0 + 1.5', 2.5",
+        "'1 * 2', 2.0",
+        "'1 / 2', 0.5",
+        "'2 + 3 * 4 / 2', 10.0",
+    )
+    @DisplayName("계산 술식을 입력하여 그 값이 정상적인지 테스트합니다.")
+    fun testCalculate(
+        input: String?,
+        output: Double?,
+    ) {
+        val calculator = Calculator()
+
+        assertThat(input?.let { calculator.calculate(it) }).isEqualTo(output)
+    }
+
+    @DisplayName("잘못된 술식을 입력하여 IllegalArgumentException이 정상적으로 발생함을 테스트합니다")
+    @ParameterizedTest
+    @ValueSource(strings = ["=", "1 a 1", "1 -", "- 1", "1+1", "1+ 1", "1 1"])
+    fun testCalulateException(input: String?) {
+        val calculator = Calculator()
+        assertThatIllegalArgumentException().isThrownBy { input?.let { calculator.calculate(it) } }
+    }
+}
